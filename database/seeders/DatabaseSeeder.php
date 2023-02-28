87a0d28fab1d252f87a022b2f0f19dfda46dde80
Andrey Khomyakov <yourwebwizard@gmail.com>
2023-02-28 15:45:37 +0300

    ingredients model

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             NovaUserSeeder::class,
+            IngredientSeeder::class,
         ]);
 
         // \App\Models\User::factory(10)->create();
---
+++ database/seeders/IngredientSeeder.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Ingredient;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class IngredientSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $ingredients = [
+            'Aqua',
+            'Glycerin',
+            'Glycerin Stearate',
+            'Butyrospermum Parkii Butter',
+            'Cetyl Palmitate',
+            'Olus Oil',
+            'Celyl Alcohol',
+            'Isopropyl Palmitate',
+            'Aloe Barbadensis Leaf Juice Powder',
+            'Dimethicone',
+            'Sodium Polyacrylate',
+            'Phenoxyethanol',
+            'Ethylhexylglycerin',
+            'Parfum',
+            /// 2
+            'Sodium Benzoate',
+            'Propylene Glycol',
+            'Sorbitol',
+            'Poloxamer 407',
+            'Sodium Lauryl Sulfate',
+            'Eucalyptol',
+            'Benzoic Acid',
+            'Methyl Salicylate',
+            'Thymol',
+            'Sodium Saccharin',
+            'Sodium Fluoride',
+            'Menthol',
+            'Sucralose',
+            'CI 42053',
+            'фторид натрия',
+            /// 3
+            'Paraffinum Liquidum',
+            'mineral oil',
+            'Cera Microcristallina',
+            'Microcrystalline wax',
+            'Lanolin Alocohol',
+            'Eticerit',
+            'Paraffin',
+            'Panthenol',
+            'Decyl Oleate',
+            'Octyldodecanol',
+            'Alumimum Stearates',
+            'Citric Acid',
+            'Magnesium Sulfate',
+            'Magnesium Stearate',
+            'Limonene',
+            'Geraniol',
+            'гераниол',
+            'Hydroxycitronellal',
+            'Linalool',
+            'Citronellol',
+            'Benzyl Benzoate',
+            'Cinnamyl alcohol',
+            ///// 4
+            'Ceteareth',
+            'cetearyl alcohol',
+            'emulsifying wax',
+            'Cocos Nucifera oil',
+            'niacinamide',
+            'ascorbyl palmitate',
+            'tocopheryl acetate',
+            'd-panthenol',
+            'hyaluronic acid',
+            'diazolidinyl urea',
+            'methylparaben',
+            'ethylparaben',
+            'propylparaben',
+            ///// 5
+            'vaselin',
+            'triethanolamine',
+            'propanediol',
+            'caprylic',
+            'capric triglyceride',
+            'dicaprylyl carbonate',
+            'dicaprylyl ether',
+        ];
+        
+        Ingredient::insert(array_map(fn($ingredient) => ['names' => $ingredient], $ingredients));
+    }
+}
